aad4c04ecfca2bd6f262cefddaeee213a3668f7f
I-Skingley <ijskingley@gmail.com>
2022-08-27 19:09:05 +0200

    Upload older tutorial code

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @toggle-add-task="$emit('toggle-add-task')" text="Add Task" color="green"/> <!--Capital B as it's component not just button tag-->
+    </header>
+</template>
+
+<!-- Array Form
+<script>
+    export default {
+        name: 'Header',
+        props: ['title'],   
+    }
+</script>
+-->
+
+<!-- Object form-->
+<script>
+    import Button from './Button' //Same folder so ./
+
+    export default {
+        name: 'Header',
+        props: {
+            title: String,
+        },
+        components:{
+            Button
+        },   
+        methods: {
+            onClick() {
+                console.log('click')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    header{
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
